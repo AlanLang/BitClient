c17e06d6fbcd4428eaae149cdd4daa7cfc5df3bf
alan <langwdalan@gmail.com>
2021-11-24 21:56:00 +0800

    ✨ 允许从剪切板获取磁力链接

--- BitClient/View/AddTorrent/AddTorrentView.swift
+++ BitClient/View/AddTorrent/AddTorrentView.swift
@@ -7,9 +7,20 @@
 
 import SwiftUI
 
+func regexGetSub(pattern:String, str:String) -> String {
+    var subStr = ""
+    let regex = try! NSRegularExpression(pattern: pattern, options:[])
+    let matches = regex.matches(in: str, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, str.count))
+    if let match = matches.first {
+        subStr = String(str[Range(match.range(at: 0), in: str)!])
+    }
+    return subStr
+}
 
 struct AddTorrentLinkView: View {
     @ObservedObject private var addTorrentLinkFormModel: AddTorrentLinkFormModel
+    @State private var showAlert = false
+    @State private var copyUrl = ""
     
     init(defaultSavePath: String) {
         UITableView.appearance().sectionFooterHeight = 8
@@ -32,32 +43,60 @@ struct AddTorrentLinkView: View {
                 Toggle("按顺序下载", isOn: $addTorrentLinkFormModel.sequentialDownload)
                 Toggle("跳过哈希值", isOn: $addTorrentLinkFormModel.skip_checking)
             }
-            
-            Section(header: Button(action: {
-                if(addTorrentLinkFormModel.urls == "") {
-                    Message.warning(message: "下载地址不能为空", title: "磁力下载")
-                }else {
-                    addTorrentLinkFormModel.download() { result in
-                        if(result) {
-                            self.mode.wrappedValue.dismiss()
+        }
+        .toolbar{
+            ToolbarItem(placement: .navigationBarTrailing) {
+                HStack {
+                    Button(action: {
+                        if(addTorrentLinkFormModel.urls == "") {
+                            Message.warning(message: "下载地址不能为空", title: "磁力下载")
+                        }else {
+                            addTorrentLinkFormModel.download() { result in
+                                if(result) {
+                                    self.mode.wrappedValue.dismiss()
+                                }
+                            }
                         }
-                    }
+                    }, label: {
+                        Image(systemName: "square.and.arrow.down")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                            .padding(.horizontal, 8)
+                            .padding(.bottom, 4)
+                            .foregroundColor(.blue)
+                    })
                 }
-            }) {
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Image(systemName: "trash")
-                        Text("开始下载")
-                        Spacer()
-                    }
-                    Spacer()
+            }
+        }
+        .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("提示"),
+                    message: Text("检测到您复制了磁力链接，是否直接添加到下载？"),
+                    primaryButton: .default(
+                        Text("不要"),
+                        action: {
+                            showAlert = false
+                        }
+                    ),
+                    secondaryButton: .default(
+                        Text("添加"),
+                        action: {
+                            showAlert = false
+                            addTorrentLinkFormModel.urls = copyUrl
+                        }
+                    )
+                )
+        }
+        .onAppear() {
+            copyUrl = ""
+            if UIPasteboard.general.hasStrings {
+                let url = regexGetSub(pattern: "magnet:\\?xt=urn:btih:[0-9a-fA-F]{40}", str: UIPasteboard.general.string ?? "")
+                if(url != "") {
+                    showAlert = true
+                    copyUrl = url
                 }
-                .foregroundColor(.white)
-                .background(Color.blue)
-                .frame(height: 35)
-            }){}
+            }
         }
     }
 }
--- BitClient/View/Service/ServiceEditView.swift
+++ BitClient/View/Service/ServiceEditView.swift
@@ -22,53 +22,38 @@ struct ServiceEditView: View {
                 TextField("服务器地址", text: $serviceEditViewModel.url)
                 TextField("用户名", text: $serviceEditViewModel.username)
                 SecureField("密码",text: $serviceEditViewModel.password)
+                Button("测试连接", action: {
+                    if (serviceEditViewModel.url == "") {
+                        Message.warning(message: "服务器地址不能为空")
+                    } else if (serviceEditViewModel.username == "") {
+                        Message.warning(message: "用户名不能为空")
+                    } else if (serviceEditViewModel.password == "") {
+                        Message.warning(message: "密码不能为空")
+                    } else {
+                        serviceEditViewModel.test()
+                    }
+                })
             }
-            Section(header: Button(action: {
-                serviceEditViewModel.test()
-            }) {
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Image(systemName: "personalhotspot")
-                        Text("测试连接")
-                        Spacer()
-                    }
-                    Spacer()
-                }
-                .foregroundColor(.black)
-                .background(Color.white)
-                .frame(height: 35)
-            }){}
-            Section(header: Button(action: {
-                if (serviceEditViewModel.url == "") {
-                    Message.warning(message: "服务器地址不能为空")
-                } else if (serviceEditViewModel.username == "") {
-                    Message.warning(message: "用户名不能为空")
-                } else if (serviceEditViewModel.password == "") {
-                    Message.warning(message: "密码不能为空")
-                } else {
-                    serviceEditViewModel.save()
-                    self.mode.wrappedValue.dismiss()
-                }
-                
-            }) {
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Image(systemName: "simcard")
-                        Text("保存设置")
-                        Spacer()
-                    }
-                    Spacer()
-                }
-                .foregroundColor(.white)
-                .background(Color.blue)
-                .frame(height: 35)
-            }){}.offset(y: -20)
         }
         .navigationBarTitle(Text("服务器配置"))
+        .toolbar{
+            ToolbarItem(placement: .navigationBarTrailing) {
+                HStack {
+                    Button(action: {
+                        serviceEditViewModel.save()
+                        self.mode.wrappedValue.dismiss()
+                    }, label: {
+                        Image(systemName: "externaldrive.badge.checkmark")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                            .padding(.horizontal, 6)
+                            .padding(.bottom, 5)
+                            .foregroundColor(.blue)
+                    })
+                }
+            }
+        }
     }
 }
 
